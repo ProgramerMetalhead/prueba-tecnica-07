f9ec83dc2fc696d57f326774d6da2addccd4d22f
ProgramerMetalhead <Cristobal343@outlook.com>
2026-09-07 16:53:01 -0500

    implementacion de formrequest

--- prueba-tecnia-api/app/Http/Requests/StoreProspectResquest.php
+++ prueba-tecnia-api/app/Http/Requests/StoreProspectResquest.php
@@ -12,7 +12,7 @@ class StoreProspectResquest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- prueba-tecnia-api/database/migrations/2026_09_07_185256_create_prospects_table.php
+++ prueba-tecnia-api/database/migrations/2026_09_07_185256_create_prospects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone')->unique();
             $table->string('status')->default(true);
-            $table->timestamp();
+            $table->timestamps();
         });
     }
 
